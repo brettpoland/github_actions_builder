4ab7157a9b4f5596a3a3e6191997d2b9e8eeddc9
brettpoland <brett.poland1@gmail.com>
2025-08-22 22:10:23 -0600

    git ignore

--- src/main/java/com/example/githubactionsbuilder/GitHubActionsBuilderApp.java
+++ src/main/java/com/example/githubactionsbuilder/GitHubActionsBuilderApp.java
@@ -7,10 +7,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.FileChooser;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
